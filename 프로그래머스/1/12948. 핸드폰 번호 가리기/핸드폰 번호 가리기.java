b39e8c519feac56e567ffbe19e9871d41307feeb
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-02 15:28:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.61 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        int len = phone_number.length();
+        answer = "*".repeat(len - 4) + phone_number.substring(len - 4);
+        return answer;
+    }
+}
