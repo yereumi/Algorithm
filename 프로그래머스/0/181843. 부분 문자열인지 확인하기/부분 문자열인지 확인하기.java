8372b0ce7f79ff3037e1ab39cbc62c17e274f54e
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-03 19:20:12 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.08 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String target) {
+        if (my_string.contains(target))
+            return 1;
+        return 0;
+    }
+}
