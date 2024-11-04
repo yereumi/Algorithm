cda28e410009e0580b6ad60c5f9849d0d1be0b7a
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-04 15:29:17 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 10.09 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -2,12 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
+        if (arr.length == 1) return new int[]{-1};
         int min =  Arrays.stream(arr).min().getAsInt();
-        if (arr.length != 1) {
-            return Arrays.stream(arr)
-                .filter(n -> n != min)
-                .toArray();
-        }
-        return new int[]{-1};
+        return Arrays.stream(arr).filter(n -> n != min).toArray();
     }
 }
