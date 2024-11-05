d66f966507eeb2cdb364d1e5ec4cf6c4a095c356
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-05 10:02:31 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        myString = myString.replace("A", "D");
-        myString = myString.replace("B", "A");
-        myString = myString.replace("D", "B");
-        
+        myString = myString.replace("A", "D").replace("B", "A").replace("D", "B");
         return myString.contains(pat) ? 1 : 0;
     }
 }
