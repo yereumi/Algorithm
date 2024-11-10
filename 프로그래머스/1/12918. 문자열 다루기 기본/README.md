0bb664c60b87d94186ca53913347370ac2a48be9
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-10 16:58:16 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/README.md
+++ 프로그래머스/1/12918. 문자열 다루기 기본/README.md
@@ -1,6 +1,6 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918?itm_content=course14743) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 10일 16:58:09
+2024년 11월 10일 16:58:14
 
 ### 문제 설명
 
--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,10 +2,8 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String s) {
-        if (s.length() != 4 && s.length() != 6)
-            return false;
         try {
-            Integer n = Integer.parseInt(s);
+            Integer.parseInt(s);
             return true;
         } catch (NumberFormatException e) {
             return false;
