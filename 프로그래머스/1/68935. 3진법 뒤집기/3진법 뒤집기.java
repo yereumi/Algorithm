df4b709cfcdcd9280baa7fa0d1bcd25faab89d50
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-11 22:04:54 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.12 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int n) {
+        StringBuffer sb = new StringBuffer(Integer.toString(n, 3));
+        return Integer.parseInt(sb.reverse().toString(), 3);
+    }
+}
