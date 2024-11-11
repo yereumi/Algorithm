0995dadec05e0ed1be87c77430f2ee6f5e87c61c
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-11 17:43:19 +0900

    [D3] Title: 조별과제, Time: 215 ms, Memory: 24,576 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/README.md
@@ -0,0 +1,15 @@
+# [D3] 조별과제 - 13218 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXzjvCCq-PwDFASs) 
+
+### 성능 요약
+
+메모리: 24,576 KB, 시간: 215 ms, 코드길이: 428 Bytes
+
+### 제출 일자
+
+2024-11-11 17:43
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/13218. 조별과제/조별과제.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{Scanner sc = new Scanner(System.in);
+		int T, N, num;
+		T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			N = sc.nextInt();
+			num = 0;
+			
+			if (N <= 2) {
+				num = 0;
+			} else {
+				num = N / 3;
+			}
+			System.out.println("#" + test_case + " " + num);
+		}
+	}
+}
