3c2a07b2520c0cad89e64d7a645534a27f12665c
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-12 09:27:21 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 11.29 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        for (int i = 0; i < t.length() - p.length() + 1; i++) {
+            String str = t.substring(i, i + p.length());
+            if (Long.parseLong(str) <= Long.parseLong(p)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
