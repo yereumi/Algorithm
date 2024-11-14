9ed217f3ec9e959b1cf028270818135e4b63cddb
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-14 23:19:00 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 14.47 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,32 +1,32 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-	        int i = 0;
-	        if (s.isBlank()) {
-	            return s;
-	        }
-	        while (i < s.length()) {
-	        	if (s.charAt(i) == ' ') {
-	        		answer += s.charAt(i);
+	    int i = 0;
+	    if (s.isBlank()) {
+	        return s;
+        }
+        while (i < s.length()) {
+        	if (s.charAt(i) == ' ') {
+	        	answer += s.charAt(i);
+	        	i++;
+	    	} else if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+	    		answer += s.charAt(i);
+        		i++;
+	        	while (i < s.length() && s.charAt(i) != ' ') {
+	        		answer += Character.toLowerCase(s.charAt(i));
+	    			i++;
+	    		}
+        	} else {
+                if (i ==0 || s.charAt(i - 1) == ' ') {
+	        		answer += Character.toUpperCase(s.charAt(i));
 	        		i++;
-	        	} else if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-	        		answer += s.charAt(i);
-	        		i++;
-	        		while (i < s.length() && s.charAt(i) != ' ') {
-	        			answer += Character.toLowerCase(s.charAt(i));
-	        			i++;
-	        		}
-	        	} else {
-	        		if (i ==0 || s.charAt(i - 1) == ' ') {
-	        			answer += Character.toUpperCase(s.charAt(i));
-	        			i++;
-	        		}
-	        		while (i < s.length() && s.charAt(i) != ' ') {
-	        			answer += Character.toLowerCase(s.charAt(i));
-	        			i++;
-	        		}
 	        	}
+	    		while (i < s.length() && s.charAt(i) != ' ') {
+	    			answer += Character.toLowerCase(s.charAt(i));
+        			i++;
+                }
 	        }
-	        return answer;
+	    }
+	    return answer;
     }
 }
