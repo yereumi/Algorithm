ea3ec18cd42c0b98b383d9904ba9dc2f4db969cb
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-17 01:28:37 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 141 ms, Memory: 22,936 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int T = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= 10; i++) {
+			String num = br.readLine();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			Deque<Integer> dq = new ArrayDeque<>();
+			for (int j = 0; j < 8; j++) {
+				dq.offer(Integer.parseInt(st.nextToken()));
+			}
+			int cycle = 1;
+			while (dq.peekLast() != 0) {
+				if (cycle == 6) {
+					cycle = 1;
+				}
+				int next = dq.poll() - cycle;
+				if (next <= 0) {
+					next = 0;
+				}
+				dq.offer(next);
+				cycle++;
+			}
+			System.out.print("#" + i + " ");
+			for (int j = 0; j < 8; j++) {
+				System.out.print(dq.poll() + " ");
+			}
+		}
+	}
+}
