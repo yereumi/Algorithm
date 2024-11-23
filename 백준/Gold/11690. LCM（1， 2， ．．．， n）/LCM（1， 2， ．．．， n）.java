238462f9ea53601fe4ac08a9295c09062e2577a0
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-24 00:39:21 +0900

    [Gold III] Title: LCM(1, 2, ..., n), Time: 2888 ms, Memory: 273868 KB -BaekjoonHub

---
+++ 백준/Gold/11690. LCM（1， 2， ．．．， n）/LCM（1， 2， ．．．， n）.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		boolean isNotPrime[] = new boolean[n + 1];
+		List<Integer> primes = new ArrayList<>();
+		for (int i = 2; i <= n; i++) {
+			if (!isNotPrime[i]) {
+				primes.add(i);
+				for (int j = i + i; j <= n; j = j + i) {
+					isNotPrime[j] = true;
+				}
+			}
+		}
+		long lcm = 1;
+		long cnt = 0;
+		for (int prime : primes) {
+			long power = prime;
+			while (power * prime <= n) {
+				power *= prime;
+			}
+			lcm = (lcm * (power % (long) Math.pow(2, 32)) % (long) Math.pow(2, 32));
+		}
+		System.out.println(lcm);
+	}
+
+	public static int gcd(int a, int b) {
+		if (a == 0)
+			return b;
+		return gcd(b % a, a);
+	}
+}
