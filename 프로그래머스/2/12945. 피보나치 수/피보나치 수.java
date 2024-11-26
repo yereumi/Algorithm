677642805eec2b00b4394a97eb2fe57a18bfb6b1
yereumi <91780220+yereumi@users.noreply.github.com>
2024-11-26 09:30:03 +0900

    [level 2] Title: 피보나치 수, Time: 3.19 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int[] f = new int [n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = (f[i - 2] + f[i - 1]) % 1234567;
+        }
+        return f[n] % 1234567;
+    }
+}
