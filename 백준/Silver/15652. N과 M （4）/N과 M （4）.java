8e42c8bc3009a50b14190a890eb32f6a3f2379d8
yereumi <91780220+yereumi@users.noreply.github.com>
2024-12-18 21:34:11 +0900

    [Silver III] Title: N과 M (4), Time: 172 ms, Memory: 19780 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static int n, m;
+	public static int[] answer;
+	public static StringBuilder sb;
+	
+	public static void recursive(int depth, int num) {
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(answer[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = num; i <= n; i++) {
+			answer[depth] = i;
+			recursive(depth + 1, i);
+		}
+	}
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        answer = new int[m];
+        recursive(0, 1);
+        System.out.println(sb);
+    }
+}
