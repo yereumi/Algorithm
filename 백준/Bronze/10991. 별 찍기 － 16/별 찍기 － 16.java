fdb3c257f5dc065cbd565c7abc229ee6952f4ebf
yereumi <91780220+yereumi@users.noreply.github.com>
2024-12-24 21:52:01 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 112 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n - 1; j++) {
+				sb.append(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				sb.append("*");
+				sb.append(" ");
+			}
+			
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
