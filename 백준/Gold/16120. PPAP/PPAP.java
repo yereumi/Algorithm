06fbc72fae8b666ecfd584f52179fb83c0f3f659
예름 <91780220+yereumi@users.noreply.github.com>
2025-01-13 16:58:21 +0900

    [Gold IV] Title: PPAP, Time: 724 ms, Memory: 157760 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.java
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.regex.Pattern;
 import java.io.*;
 
 public class Main {
@@ -9,21 +8,18 @@ public class Main {
 		String str = br.readLine();
 		Deque<String> dq = new ArrayDeque<>();
 		
-		int cnt = 0;
 		for (int i = 0; i < str.length(); i++) {
 			dq.offer(String.valueOf(str.charAt(i)));
+			
 			if (dq.size() >= 4) {
                 String fourth = dq.pollLast();
                 String third = dq.pollLast();
                 String second = dq.pollLast();
                 String first = dq.pollLast();
 
-                // "PPAP" 패턴인지 확인
                 if (first.equals("P") && second.equals("P") && third.equals("A") && fourth.equals("P")) {
-                    // "PPAP"를 "P"로 치환
                     dq.offer("P");
                 } else {
-                    // 패턴이 아니라면 다시 넣기
                     dq.offer(first);
                     dq.offer(second);
                     dq.offer(third);
