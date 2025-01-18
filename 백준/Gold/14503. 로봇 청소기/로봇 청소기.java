5ca0bcc60f61a6869d1914488c9423c2d8e8f3e2
예름 <91780220+yereumi@users.noreply.github.com>
2025-01-18 20:51:19 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -3,11 +3,11 @@ import java.io.*;
 
 public class Main {
 	
-	static int n, m, cnt = 1;
+	static int n, m, cnt = 0;
 	static int[][] board;
 	static boolean[][] visited;
 	static int[] dy = { -1, 0, 1, 0 }; // 북, 동, 남, 서
-    static int[] dx = { 0, 1, 0, -1 };
+	static int[] dx = { 0, 1, 0, -1 };
 	
 	static boolean isValid(int y, int x) {
 		return y >= 0 && y < n && x >= 0 && x < m;
@@ -19,27 +19,27 @@ public class Main {
 	
 	public static void bfs(int y, int x, int d) {
 		if (!visited[y][x]) {
-            visited[y][x] = true;
-            cnt++;
-        }
+			visited[y][x] = true;
+			cnt++;
+		}
 
 		for (int i = 0; i < 4; i++) {
-			d = (d + 3) % 4; // 반시계 방향 회전
-            int ny = y + dy[d];
-            int nx = x + dx[d];
-
-            if (isPossible(ny, nx)) {
-                bfs(ny, nx, d);
-                return;
-            }
+			d = (d + 3) % 4;
+			int ny = y + dy[d];
+			int nx = x + dx[d];
+			if (isPossible(ny, nx)) {
+				bfs(ny, nx, d);
+				return;
+			}
 		}
 		int backDir = (d + 2) % 4;
         int ny = y + dy[backDir];
         int nx = x + dx[backDir];
 
-        if (isValid(ny, nx) && board[ny][nx] != 1) {
-            bfs(ny, nx, d);
-        }
+		if (isValid(ny, nx) && board[ny][nx] != 1) {
+			bfs(ny, nx, d);
+			return;
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -60,7 +60,6 @@ public class Main {
 		}
 		
 		visited = new boolean[n][m];
-		visited[r][c] = true;
 		bfs(r, c, d);
 		System.out.println(cnt);
 	}
