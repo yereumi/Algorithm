cd0553767e78e22fe9376026a7636697c3a1e5ef
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-01-28 23:14:28 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 128 ms, Memory: 15320 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] num = new int[n];
+		int[] length = new int[n];
+		for (int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		int maxLen = 1;
+		for (int i = 0; i < n; i++) {
+			length[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (num[j] < num[i]) {
+					length[i] = Math.max(length[i], length[j] + 1);
+					maxLen = Math.max(maxLen, length[i]);
+				}
+			}
+		}
+		sb.append(maxLen).append("\n");
+		Deque<Integer> dq = new ArrayDeque<>();
+		for (int i = n - 1; i >= 0; i--) {
+			if (length[i] == maxLen) {
+				dq.push(num[i]);
+				maxLen--;
+			}
+		}
+		while (!dq.isEmpty()) {
+			sb.append(dq.pop()).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
