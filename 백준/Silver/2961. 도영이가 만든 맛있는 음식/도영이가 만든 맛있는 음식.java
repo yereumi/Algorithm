b96c8b86c9da345be6ac244a7670074c4e1cc9a8
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-02-11 15:50:13 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -2,38 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
+
 	private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48)
-            n = (n << 3) + (n << 1) + (c & 15);
-        return n;
-    }
-	
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+
 	static int n;
 	static long min = Integer.MAX_VALUE;
 	static long[][] food;
-	
-	public static long recursive(boolean[] visited, int max, int d, int s) {
-		if (d == max) {
-			long sum = 0, multiple = 1;
-			for (int i = 0; i < n; i++) {
-				if (visited[i]) sum += food[i][1];
+
+	public static void recursive(int depth, long sSum, long bSum) {
+		if (depth == n) {
+			if (sSum + bSum != 1) {
+				min = Math.min(min, Math.abs(sSum - bSum));
 			}
-			for (int i = 0; i < n; i++) {
-				if (visited[i]) multiple *= food[i][0];
-			}
-			min = Math.min(min, Math.abs(sum - multiple));
-			return min;
+			return;
 		}
-		for (int i = s; i < n; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				min = recursive(visited, max, d + 1, s + 1);
-				visited[i] = false;
-			}
-		}
-		return min;
+		recursive(depth + 1, sSum * food[depth][0], bSum + food[depth][1]);
+		recursive(depth + 1, sSum, bSum);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -44,10 +33,7 @@ public class Main {
 			food[i][1] = read();
 		}
 		long result = 0;
-		for (int i = 1; i <= n; i++) {
-			boolean[] visited = new boolean[n];
-			result = recursive(visited, i, 0, 0);
-		}
-		System.out.println(result);
+		recursive(0, 1, 0);
+		System.out.println(min);
 	}
 }
