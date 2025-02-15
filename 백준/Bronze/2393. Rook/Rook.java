6b750960b2ede5c1326da39e8f40130b0c64b272
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-02-15 23:48:46 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println("  ___  ___  ___\n"
+				+ "  | |__| |__| |\n"
+				+ "  |           |\n"
+				+ "   \\_________/\n"
+				+ "    \\_______/\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |_____|\n"
+				+ "  __/       \\__\n"
+				+ " /             \\\n"
+				+ "/_______________\\");
+	}
+}
