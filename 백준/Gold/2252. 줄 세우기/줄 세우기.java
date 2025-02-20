c9857698c540bea365ebc4a29a5dcef5c63c4457
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-02-20 09:52:19 +0900

    [Gold III] Title: 줄 세우기, Time: 244 ms, Memory: 26660 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean m = n == 13;
+		if (m)
+			n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return m ? ~n + 1 : n;
+	}
+    
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		int n = read();
+		int m = read();
+		List<List<Integer>> graph = new ArrayList<>();
+		for (int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<>());
+		}
+		int[] edgeCount = new int[n + 1];
+		for (int i = 0; i < m; i++) {
+			int a = read();
+			int b = read();
+			graph.get(a).add(b);
+			edgeCount[b]++;
+		}
+		
+		Deque<Integer> dq = new ArrayDeque<>();
+		for (int i = 1; i <= n; i++) {
+			if (edgeCount[i] == 0) dq.offer(i);
+		}
+		while (!dq.isEmpty()) {
+			int v = dq.poll();
+			sb.append(v).append(" ");
+			for (int u : graph.get(v)) {
+				edgeCount[u]--;
+				if (edgeCount[u] == 0) dq.offer(u);
+			}
+		}
+		System.out.println(sb);
+	}
+}
