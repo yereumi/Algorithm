db7c83cbbed24b152c0490041ec3af3cda0fd727
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-02-20 10:01:53 +0900

    [Gold II] Title: 문제집, Time: 304 ms, Memory: 25500 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		int n = read();
+		int m = read();
+		List<List<Integer>> graph = new ArrayList<>();
+		for (int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<>());
+		}
+		int[] edgeCount = new int[n + 1];
+		for (int i = 0; i < m; i++) {
+			int a = read();
+			int b = read();
+			graph.get(a).add(b);
+			edgeCount[b]++;
+		}
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o1 - o2);
+		for (int i = 1; i <= n; i++) {
+			if (edgeCount[i] == 0) pq.offer(i);
+		}
+		while (!pq.isEmpty()) {
+			int v = pq.poll();
+			sb.append(v).append(" ");
+			for (int u : graph.get(v)) {
+				edgeCount[u]--;
+				if (edgeCount[u] == 0) pq.offer(u);
+			}
+		}
+		System.out.println(sb);
+	}
+}
