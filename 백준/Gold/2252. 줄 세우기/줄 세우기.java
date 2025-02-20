c7202089b15f54f63e52e727716d2100b1738ec3
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-02-20 09:54:55 +0900

    [Gold III] Title: 줄 세우기, Time: 232 ms, Memory: 26752 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,15 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	private static int read() throws Exception {
+    
+    private static int read() throws Exception {
 		int c, n = System.in.read() & 15;
-		boolean m = n == 13;
-		if (m)
-			n = System.in.read() & 15;
 		while ((c = System.in.read()) >= 48)
 			n = (n << 3) + (n << 1) + (c & 15);
-		return m ? ~n + 1 : n;
+		return n;
 	}
     
 	public static void main(String[] args) throws Exception {
