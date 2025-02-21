a8cf1ac193b87bdcfac2277e4d9c99e9d379f3ba
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-02-21 10:03:37 +0900

    [Gold IV] Title: 빙산, Time: 592 ms, Memory: 56316 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,103 +1,107 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	
+
+	/**
+	 * 1. bfs로 순회하면서 빙산의 개수 줄이기 -> 다른 이차원 배열에 저장
+	 * 2. 빙산의 개수 세기 -> boolean으로 저장해서 while문 종료 조건
+	 * 3. 전부 다 녹을 때까지 두 덩어리 이상으로 분리되지 않으면 0 출력
+	 */
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+
 	static int n, m;
 	static int[][] board;
 	static boolean[][] visited;
-	static int[] dx = { 0, 0, 1, -1 };
-	static int[] dy = { -1, 1, 0, 0 };
-	
-	static boolean isValid(int y, int x) {
+
+	static int[] dy = new int[] { 0, 0, -1, 1 };
+	static int[] dx = new int[] { -1, 1, 0, 0 };
+
+	private static boolean isValid(int y, int x) {
 		return y >= 0 && y < n && x >= 0 && x < m;
 	}
-	
-	static boolean isPossible(int y, int x, boolean[][] visited) {
-		return isValid(y, x) && !visited[y][x] && board[y][x] == 0;
-	}
-	
-	public static int count() {
-		int cnt = 0;
+
+	private static void bfs() {
 		for (int i = 0; i < n; i++) {
 			Arrays.fill(visited[i], false);
 		}
-		Deque<int[]> dq = new ArrayDeque<>();
 		
 		for (int i = 0; i < n * m; i++) {
 			int y = i / m;
 			int x = i % m;
+			if (board[y][x] == 0)
+				continue;
+
+			int cnt = 0;
+			visited[y][x] = true;
+			for (int j = 0; j < 4; j++) {
+				int ny = y + dy[j];
+				int nx = x + dx[j];
+				if (isValid(ny, nx) && !visited[ny][nx] && board[ny][nx] == 0) {
+					cnt++;
+				}
+			}
+			board[y][x] = board[y][x] - cnt < 0 ? 0 : board[y][x] - cnt;
+		}
+	}
+
+	private static int count() {
+		int cnt = 0;
+		Deque<int[]> dq = new ArrayDeque<>();
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(visited[i], false);
+		}
+		
+		for (int i = 0; i < n * m; i++) {
+			int y = i / m;
+			int x = i % m;
+			if (board[y][x] == 0 || visited[y][x]) continue;
 			
-			if (board[y][x] != 0 && !visited[y][x]) {
-				dq.offer(new int[] { y, x });
-				cnt++;
-				while (!dq.isEmpty()) {
-					int[] xy = dq.poll();
-					for (int j = 0; j < 4; j++) {
-						int ny = xy[0] + dy[j];
-						int nx = xy[1] + dx[j];
-						if (isValid(ny, nx) && !visited[ny][nx] && board[ny][nx] != 0) {
-							dq.offer(new int[] { ny, nx });
-							visited[ny][nx] = true;
-						}
+			cnt++;
+			dq.offer(new int[] { y, x });
+			while (!dq.isEmpty()) {
+				int[] now = dq.poll();
+				for (int j = 0; j < 4; j++) {
+					int ny = now[0] + dy[j];
+					int nx = now[1] + dx[j];
+					if (isValid(ny, nx) && !visited[ny][nx] && board[ny][nx] != 0) {
+						dq.offer(new int[] { ny, nx });
+						visited[ny][nx] = true;
 					}
 				}
 			}
 		}
 		return cnt;
 	}
-	
-	public static void melt() {
-		for (int i = 0; i < n; i++) {
-			Arrays.fill(visited[i], false);
-		}
-		
-		for (int i = 0; i < n * m; i++) {
-			int y = i / m;
-			int x = i % m;
-			
-			if (board[y][x] != 0) {
-				int cnt = 0;
-				visited[y][x] = true;
-				for (int j = 0; j < 4; j++) {
-					int ny = y + dy[j];
-					int nx = x + dx[j];
-					if (isPossible(ny, nx, visited)) {
-						cnt++;
-					}
-				}
-				board[y][x] -= cnt;
-				if (board[y][x] < 0) board[y][x] = 0;
-			}
-		}
-	}
-	
+
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		board = new int [n][m];
+		n = read();
+		m = read();
+		board = new int[n][m];
 		visited = new boolean[n][m];
 		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < m; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+				board[i][j] = read();
 			}
 		}
-		
-		int cnt = 0;
+        
+		int year = 0;
 		while (true) {
-			int ice = count();
-			if (ice >= 2) break;
-			if (ice <= 0) {
-				cnt = 0;
+			int cnt = count();
+			if (cnt >= 2) break;
+			if (cnt == 0) {
+				year = 0;
 				break;
 			}
-			melt();
-			cnt++;
+			bfs();
+			year++;
 		}
-		System.out.println(cnt);
-
+		System.out.println(year);
 	}
 }
