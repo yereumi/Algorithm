d4fdf290e6fa5b631d0c16df6f2f77cc6d6b2e6a
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-03-04 16:34:25 +0900

    [Gold V] Title: 강의실 배정, Time: 420 ms, Memory: 26104 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -2,26 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    
+    private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		int n = read();
 		int[][] time = new int[n][2];
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			time[i][0] = Integer.parseInt(st.nextToken());
-			time[i][1] = Integer.parseInt(st.nextToken());
+			time[i][0] = read();
+			time[i][1] = read();
 		}
-		Arrays.sort(time, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if (o1[0] == o2[0]) {
-					return o1[1] - o2[1];
-				}
-				return o1[0] - o2[0];
-			}
-		});
-		
+		Arrays.sort(time, (o1, o2) -> o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1]);
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		for (int i = 0; i < n; i++) {
 			if (!pq.isEmpty() && pq.peek() <= time[i][0]) {
@@ -29,7 +25,6 @@ public class Main {
 			}
 			pq.offer(time[i][1]);
 		}
-		
 		System.out.println(pq.size());
 	}
 }
