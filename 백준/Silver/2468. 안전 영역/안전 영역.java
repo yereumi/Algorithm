5b950c586b7c97b3d262380700bef628bc4d1478
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-03-13 11:36:48 +0900

    [Silver I] Title: 안전 영역, Time: 240 ms, Memory: 35700 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+    
 	static int read() throws Exception {
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) >= 48)
@@ -20,45 +20,48 @@ public class Main {
 		return y >= 0 && y < n && x >= 0 && x < n;
 	}
 
-	static void bfs(int height, int depth) {
-		int y = depth / n;
-		int x = depth % n;
-		if (visited[y][x] || board[y][x] <= height) return;
-
+	static void bfs(int y, int x, int height) {
 		Deque<int[]> dq = new ArrayDeque<>();
 		dq.offer(new int[] { y, x });
 		visited[y][x] = true;
+		
 		while (!dq.isEmpty()) {
 			int[] now = dq.poll();
-
 			for (int i = 0; i < 4; i++) {
 				int ny = now[0] + dy[i];
 				int nx = now[1] + dx[i];
 
-				if (isValid(ny, nx) && !visited[ny][nx]) {
-					visited[ny][nx] = true;
-					if (board[ny][nx] > height) dq.offer(new int[] { ny, nx });
-				}
+				if (isValid(ny, nx) && !visited[ny][nx] && board[ny][nx] > height) {
+	                visited[ny][nx] = true;
+	                dq.offer(new int[] { ny, nx });
+	            }
 			}
 		}
-		result++;
 	}
 
 	public static void main(String[] args) throws Exception {
+		int minHeight = Integer.MAX_VALUE, maxHeight = Integer.MIN_VALUE;
 		n = read();
 		board = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				board[i][j] = read();
+				minHeight = Math.min(minHeight, board[i][j]);
+		        maxHeight = Math.max(maxHeight, board[i][j]);
 			}
 		}
 		answer = 0;
-		for (int i = 0; i <= 100; i++) {
+		for (int i = minHeight - 1; i <= maxHeight; i++) {
 			result = 0;
 			visited = new boolean[n][n];
-			for (int j = 0; j < n * n; j++) {
-				bfs(i, j);
-			}
+			for (int y = 0; y < n; y++) {
+		        for (int x = 0; x < n; x++) {
+		            if (!visited[y][x] && board[y][x] > i) {
+		                bfs(y, x, i);
+		                result++;
+		            }
+		        }
+		    }
 			answer = Math.max(answer, result);
 		}
 		System.out.println(answer);
