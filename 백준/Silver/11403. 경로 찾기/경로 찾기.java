aa9c23b3f0ecc60de5f29b563564dfaeee264281
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-03-14 10:32:53 +0900

    [Silver I] Title: 경로 찾기, Time: 140 ms, Memory: 16120 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		int n = read();
+		int[][] board = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				board[i][j] = read();
+			}
+		}
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (board[i][j] == 1) continue;
+					board[i][j] = board[i][k] == 1 && board[k][j] == 1 ? 1 : 0;
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				sb.append(board[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
