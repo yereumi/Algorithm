b16dbd162082bdf0337c570166dfdb3f4e9a5972
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-03-20 23:47:00 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+	
+	static int n, normal, blind;
+	static char[][] color;
+	static int[] dy = new int[] { 0, 0, -1, 1 };
+	static int[] dx = new int[] { -1, 1, 0, 0 };
+	
+	static boolean isValid(int y, int x) {
+		return y >= 0 && y < n && x >= 0 && x < n;
+	}
+	
+	static void bfs() {
+		boolean[][] visited = new boolean[n][n];
+		Deque<int[]> dq = new ArrayDeque<>();
+		for (int i = 0; i < n * n; i++) {
+			int y = i / n;
+			int x = i % n;
+			if (!visited[y][x]) {
+				normal++;
+				visited[y][x] = true;
+				dq.offer(new int[] { y, x });
+				while (!dq.isEmpty()) {
+					int[] now = dq.poll();
+					
+					for (int j = 0; j < 4; j++) {
+						int ny = now[0] + dy[j];
+						int nx = now[1] + dx[j];
+						if (isValid(ny, nx) && !visited[ny][nx] && color[now[0]][now[1]] == color[ny][nx]) {
+							visited[ny][nx] = true;
+							dq.offer(new int[] { ny, nx });
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	static void blindBfs() {
+		boolean[][] visited = new boolean[n][n];
+		Deque<int[]> dq = new ArrayDeque<>();
+		for (int i = 0; i < n * n; i++) {
+			int y = i / n;
+			int x = i % n;
+			if (!visited[y][x]) {
+				blind++;
+				visited[y][x] = true;
+				dq.offer(new int[] { y, x });
+				while (!dq.isEmpty()) {
+					int[] now = dq.poll();
+					
+					if (color[now[0]][now[1]] == 'B') {
+						for (int j = 0; j < 4; j++) {
+							int ny = now[0] + dy[j];
+							int nx = now[1] + dx[j];
+							if (isValid(ny, nx) && !visited[ny][nx] && color[now[0]][now[1]] == color[ny][nx]) {
+								visited[ny][nx] = true;
+								dq.offer(new int[] { ny, nx });
+							}
+						}
+					} else {
+						for (int j = 0; j < 4; j++) {
+							int ny = now[0] + dy[j];
+							int nx = now[1] + dx[j];
+							if (isValid(ny, nx) && !visited[ny][nx] && (color[ny][nx] == 'G' || color[ny][nx] == 'R')) {
+								visited[ny][nx] = true;
+								dq.offer(new int[] { ny, nx });
+							}
+						}
+					}
+					
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = read();
+		color = new char[n][n];
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < n; j++) {
+				color[i][j] = str.charAt(j);
+			}
+		}
+		normal = blind = 0;
+		bfs();
+		blindBfs();
+		System.out.println(normal + " " + blind);
+	}
+}
