b3c1cf34eaf0bef79a9b4bc94ed6baaf00d01e23
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-03-20 23:51:03 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -2,13 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) >= 48)
-			n = (n << 3) + (n << 1) + (c & 15);
-		return n;
-	}
 	
 	static int n, normal, blind;
 	static char[][] color;
@@ -85,7 +78,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = read();
+		n = Integer.parseInt(br.readLine());
 		color = new char[n][n];
 		for (int i = 0; i < n; i++) {
 			String str = br.readLine();
