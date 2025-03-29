c672b580c5d14d792bd98f0a60bcb6c5371f1401
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-03-29 23:11:47 +0900

    [Bronze V] Title: AI 선도대학, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(read() - 2024);
+	}
+}
