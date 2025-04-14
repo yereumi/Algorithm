b5878c5bd6291e452e2ebc64fc13ca0f6530eed7
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-04-14 10:12:09 +0900

    [Silver III] Title: 수리공 항승, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -0,0 +1,42 @@
+
+/**
+ * 시간 2초: 2억번 안에 연산
+ * 최대 메모리 256MB: int 기준 대략 256 * 1000 * 1000 / 4 = 64_000_000개 할당 가능
+ * 1<=N, L<=1000 -> O(N^3)까지 가능
+ */
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int l = Integer.parseInt(st.nextToken());
+		int[] water = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			water[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(water);
+		int answer = 0, len = 0;
+		for (int i = 0; i < n - 1; i++) {
+			int distance = water[i + 1] - water[i];
+			if (distance >= l) {
+				answer++;
+				if (i == n - 2) answer++;
+				len = 0;
+			} else if (distance + len >= l) {
+				answer++;
+				if (i == n - 2) answer++;
+				len = 0;
+			} else {
+				len += distance;
+			}
+		}
+		if (len != 0) answer++;
+		if (l == 1) answer = n;
+		if (n == 1) answer = 1;
+		System.out.println(answer);
+	}
+}
