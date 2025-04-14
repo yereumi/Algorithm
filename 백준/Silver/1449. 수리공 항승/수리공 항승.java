488c68c892621fcc653e0f5ca6d0d498738e1aaa
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-04-14 10:13:29 +0900

    [Silver III] Title: 수리공 항승, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.java
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -8,15 +8,19 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int l = Integer.parseInt(st.nextToken());
+		int n = read();
+		int l = read();
 		int[] water = new int[n];
-		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			water[i] = Integer.parseInt(st.nextToken());
+			water[i] = read();
 		}
 		Arrays.sort(water);
 		int answer = 0, len = 0;
