e3380fb8742cec6ea0dbe893190814b8ceaa919c
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-04-16 11:16:42 +0900

    [Gold V] Title: 주사위, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Gold/1041. 주사위/주사위.java
+++ 백준/Gold/1041. 주사위/주사위.java
@@ -1,8 +1,3 @@
-/**
- * 시간 2초: 2억번 안에 연산
- * 최대 메모리 256MB: int 기준 대략 256 * 1000 * 1000 / 4 = 64_000_000개 할당 가능
- * 1<=N, L<=1000 -> O(N^3)까지 가능
- */
 import java.util.*;
 import java.io.*;
 
@@ -14,7 +9,6 @@ public class Main {
 		return n;
 	}
 	
-
 	public static void main(String[] args) throws Exception {
 		int n = read();
 		int[] num = new int[6];
@@ -43,10 +37,9 @@ public class Main {
 				Math.min(num[2] + num[5],
 				Math.min(num[3] + num[4],
 				Math.min(num[3] + num[5], num[4] + num[5])))))))))));
-//		System.out.println(threeMin + " " + twoMin + " " + oneMin);
 		long answer = threeMin * 4 + 
-				twoMin * 4 + twoMin * 8 * (n - 2) + 
-				oneMin * 4 * (n - 2) + oneMin * 5 * (n - 2) * (n - 2);
+				twoMin * (4 + 8 * (n - 2)) + 
+				oneMin * (4 * (n - 2) + (long)5 * (n - 2) * (n - 2));
 		if (n >= 2) System.out.println(answer);
 		else System.out.println(sum - oneMax);
 	}
