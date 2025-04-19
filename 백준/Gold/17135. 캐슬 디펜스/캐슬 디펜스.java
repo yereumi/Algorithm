067ab588a3a18815be97e0b2a247f11d57281127
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-04-19 17:16:44 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 320 ms, Memory: 44456 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,19 +1,7 @@
-/**
- * 시간 2초: 2억번 안에 연산
- * 최대 메모리 256MB: int 기준 대략 256 * 1000 * 1000 / 4 = 64_000_000개 할당 가능
- * 1<=N, L<=1000 -> O(N^3)까지 가능
- */
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) >= 48)
-			n = (n << 3) + (n << 1) + (c & 15);
-		return n;
-	}
-	
 	static int n, m, d;
 	static int[][] board, copyBoard;
 	static int[] dy = new int[] { 0, -1, 0 };
@@ -70,24 +58,22 @@ public class Main {
 		int enemy = 0;
 		copyBoard = new int[n][m];
 		copyBoard();
-//		System.out.println("===" + eros[0] + " " + eros[1] + " " + eros[2] + "===");
 		for (int i = 0; i < n; i++) {
 			Set<String> enemies = new HashSet<>();
 			for (int j = 0; j < 3; j++) {
 				int[] e = findEnemy(eros[j]);
 				if (e[0] == -1 && e[1] == -1) continue;
 				enemies.add(e[0] + "," + e[1]);
-//				System.out.print(e[0] + "," + e[1] + " ");
 			}
 			enemy += enemies.size();
 			for (String e : enemies) {
 				String[] yx = e.split(",");
-				copyBoard[Integer.parseInt(yx[0])][Integer.parseInt(yx[1])] = 0;
+                int r = Integer.parseInt(yx[0]);
+                int c = Integer.parseInt(yx[1]);
+				copyBoard[r][c] = 0;
 			}
 			moveBoard();
-//			System.out.println();
 		}
-//		System.out.println(enemy);
 		return enemy;
 	}
 	
