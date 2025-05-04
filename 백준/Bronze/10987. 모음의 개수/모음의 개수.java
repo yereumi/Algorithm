aab7c332189c50849f2896a85c348a03cb893e2c
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-05-04 23:41:10 +0900

    [Bronze IV] Title: 모음의 개수, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int answer = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+				answer++;
+		}
+		System.out.println(answer);
+	}	
+}
