76b7679fdbb49184b1da4078f977dff1a9ae4082
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-05-18 23:48:10 +0900

    [Bronze IV] Title: 모비스, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/28074. 모비스/모비스.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		if (str.contains("M") && str.contains("O") && str.contains("B") && str.contains("I") && str.contains("S")) System.out.println("YES");
+		else System.out.println("NO");
+	}
+}
