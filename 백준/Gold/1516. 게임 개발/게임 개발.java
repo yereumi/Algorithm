73bdc1c5b8f7571f0506512fd0739dfc3c211308
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-05-21 10:14:47 +0900

    [Gold III] Title: 게임 개발, Time: 228 ms, Memory: 21848 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -4,7 +4,9 @@ import java.io.*;
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
+        
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
 		int[] time = new int[n + 1];
 		int[] result = new int[n + 1];
@@ -17,6 +19,7 @@ public class Main {
 		for (int i = 1; i <= n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			time[i] = result[i] = Integer.parseInt(st.nextToken());
+            
 			while (true) {
 				int node = Integer.parseInt(st.nextToken());
 				if (node == -1) break;
@@ -42,7 +45,8 @@ public class Main {
 		}
 		
 		for (int i = 1; i <= n; i++) {
-			System.out.println(result[i]);
+			sb.append(result[i]).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
