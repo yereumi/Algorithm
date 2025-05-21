346ca5a1dbd7921af9332534ea04b5a86e49b9af
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-05-21 10:08:45 +0900

    [Gold III] Title: 게임 개발, Time: 252 ms, Memory: 22644 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] time = new int[n + 1];
+		int[] result = new int[n + 1];
+		int[] degree = new int[n + 1];
+		List<List<Integer>> graph = new ArrayList<>();
+		for (int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			time[i] = result[i] = Integer.parseInt(st.nextToken());
+			while (true) {
+				int node = Integer.parseInt(st.nextToken());
+				if (node == -1) break;
+				degree[i]++;
+				graph.get(node).add(i);
+			}
+		}
+		
+		Deque<Integer> dq = new ArrayDeque<>();
+		for (int i = 1; i <= n; i++) {
+			if (degree[i] == 0) dq.offer(i);
+		}
+		
+		while (!dq.isEmpty()) {
+			int now = dq.poll();
+			List<Integer> nodes = graph.get(now);
+			
+			for (int node : nodes) {
+				degree[node]--;
+				if (degree[node] == 0) dq.offer(node);
+				result[node] = Math.max(result[node], result[now] + time[node]);
+			}
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println(result[i]);
+		}
+	}
+}
