add202ade1e6c52ccece23b1cc3b24e22ec27b45
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-05-27 10:02:49 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 512 ms, Memory: 60648 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 	
-	static int k, w, h, min;
+	static int k, w, h;
 	static int[][] board;
 	static int[] dr = new int[] { -1, 1, 0, 0 };
 	static int[] hr = new int[] { 1, 2, 1, 2, -1, -2, -1, -2 };
@@ -21,14 +21,14 @@ public class Main {
 		
 		while (!dq.isEmpty()) {
 			int[] now = dq.poll();
-			
+			if (now[0] == h - 1 && now[1] == w - 1) return now[3];
+
 			for (int i = 0; i < 4; i++) {
 				int nr = now[0] + dr[i];
 				int nc = now[1] + dc[i];
 				int nh = now[2];
 				int nd = now[3] + 1;
 				
-				if (nr == h - 1 && nc == w - 1) min = Math.min(min, nd);
 				if (isValid(nr, nc) && board[nr][nc] == 0 && !visited[nr][nc][nh]) {
 					dq.offer(new int[] { nr, nc, nh, nd });
 					visited[nr][nc][nh] = true;
@@ -42,19 +42,15 @@ public class Main {
 					int nh = now[2] + 1;
 					int nd = now[3] + 1;
 					
-					if (nr == h - 1 && nc == w - 1) min = Math.min(min, nd);
 					if (isValid(nr, nc) && board[nr][nc] == 0 && !visited[nr][nc][nh]) {
 						dq.offer(new int[] { nr, nc, nh, nd });
 						visited[nr][nc][nh] = true;
 					}
 				}
 			}
-			
 		}
 		
-		if (w == 1 && h == 1) return 0;
-		if (min == Integer.MAX_VALUE) return -1;
-		return min;
+		return -1;
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -72,7 +68,6 @@ public class Main {
 			}
 		}
 		
-		min = Integer.MAX_VALUE;
 		System.out.println(bfs());
 	}
 }
