ebba5f76623f70a18477bca6a17b2414815583bb
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-07-10 00:02:12 +0900

    [Bronze V] Title: A+B - 9, Time: 344 ms, Memory: 21404 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger A = new BigInteger(sc.next());
+        BigInteger B = new BigInteger(sc.next());
+
+        System.out.println(A.add(B));
+    }
+}
