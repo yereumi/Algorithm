fd5512a13af3c82214f076aa95d89da2dbfa98d8
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-08-14 00:40:55 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong(), m = sc.nextLong(), k = sc.nextLong();
+        System.out.println(m * (k + 1));
+    }
+}
