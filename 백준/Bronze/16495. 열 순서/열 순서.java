5379b97d3a7bd666e8a9b1628e09ab51c115b4cb
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-10-19 22:48:27 +0900

    [Bronze I] Title: 열 순서, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/16495. 열 순서/열 순서.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine().trim();
+        
+        long result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result = result * 26 + (s.charAt(i) - 'A' + 1);
+        }
+        
+        System.out.println(result);
+    }
+}
