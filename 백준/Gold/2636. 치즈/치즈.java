3bce7abc88735a38c4820b1870e8a901cb01b9ee
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-10-25 17:34:09 +0900

    [Gold IV] Title: 치즈, Time: 128 ms, Memory: 15324 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,89 +1,86 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) >= 48)
-			n = (n << 3) + (n << 1) + (c & 15);
-		return n;
-	}
 	
 	static int n, m;
-	static int[][] cheese;
-	static int[] dy = new int[] { -1, 1, 0, 0 };
-	static int[] dx = new int[] { 0, 0, -1, 1 };
+	static int[][] board;
+	static boolean[][] visited;
+	static int[] dr = new int[] { -1, 1, 0, 0 };
+	static int[] dc = new int[] { 0, 0, -1, 1 };
 	
-	static boolean isValid(int y, int x) {
-		return y >= 0 && y < n && x >= 0 && x < m;
+	static boolean isValid(int r, int c) {
+		return r >= 0 && r < n && c >= 0 && c < m;
 	}
 	
-	static void find() {
+	private static int piece() {
+		int answer = 0;
+		
+		visited = new boolean[n][m];
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (board[i][j] == 1 && !visited[i][j]) {
+					answer++;
+				}
+				visited[i][j] = true;
+			}
+		}
+		
+		return answer;
+	}
+	
+	private static void melt() {
+		visited = new boolean[n][m];
 		Deque<int[]> dq = new ArrayDeque<>();
 		dq.offer(new int[] { 0, 0 });
-		boolean[][] visited = new boolean[n][m];
 		visited[0][0] = true;
 		
 		while (!dq.isEmpty()) {
 			int[] now = dq.poll();
-			int nowY = now[0];
-			int nowX = now[1];
 			
 			for (int i = 0; i < 4; i++) {
-				int nextY = nowY + dy[i];
-				int nextX = nowX + dx[i];
-				if (isValid(nextY, nextX) && !visited[nextY][nextX]) {
-					if (cheese[nextY][nextX] == 0) {
-						visited[nextY][nextX] = true;
-						dq.offer(new int[] { nextY, nextX });
+				int nr = now[0] + dr[i];
+				int nc = now[1] + dc[i];
+				
+				if (isValid(nr, nc) && !visited[nr][nc]) {
+					if (board[nr][nc] == 0) {
+						dq.offer(new int[] { nr, nc });
+					} else {
+						board[nr][nc] = 0;
 					}
-					if (cheese[nextY][nextX] == 1) {
-						visited[nextY][nextX] = true;
-						cheese[nextY][nextX] = 2;
-					}
+					visited[nr][nc] = true;
 				}
 			}
 		}
 	}
 	
-	static void melt() {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		board = new int[n][m];
+		
 		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < m; j++) {
-				if (cheese[i][j] == 2) cheese[i][j] = 0;
-			}
-		}	
-	}
-	
-	static int count() {
-		int cnt = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (cheese[i][j] == 1) cnt++;
-			}
-		}
-		return cnt;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		n = read();
-		m = read();
-		cheese = new int[n][m];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				cheese[i][j] = read();
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		int time = 0;
-		int cnt = count();
+		int prevPiece = 0;
+		int piece = 0;
+		int year = 0;
 		while (true) {
-			find();
-			time++;
+			prevPiece = piece;
+			piece = piece();
+			if (piece == 0) break;
 			melt();
-			if (count() == 0) break;
-			cnt = count();
+			year++;
 		}
-		System.out.println(time + "\n" + cnt);
+		
+		System.out.println(year);		
+		System.out.println(prevPiece);
 	}
 }
