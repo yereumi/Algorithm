6f4e72e192d4b103e392c16f5fea747074063f0d
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-10-28 01:11:15 +0900

    [Silver III] Title: 등수 매기기, Time: 768 ms, Memory: 53392 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] rank = new int[n];
+		for (int i = 0; i < n; i++) {
+			rank[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(rank);
+		long answer = 0;
+		for (int i = 1; i <= n; i++) {
+			answer += Math.abs(rank[i - 1] - i);
+		}
+		System.out.println(answer);
+	}
+}
