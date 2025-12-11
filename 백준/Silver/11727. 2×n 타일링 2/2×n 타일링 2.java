512275db7fe4a42bc56d62196a74cc7601a1db8d
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-12-11 18:46:16 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		if (n >= 1) dp[1] = 1;
+        if (n >= 2) dp[2] = 3;
+		
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
