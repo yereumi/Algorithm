baed7ac99df118e0e61de496a89af181531b83fd
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-12-21 03:13:11 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine().trim());
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
