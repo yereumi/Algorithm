22c99ce1ad05937b512ff96c5df7272e4f29acda
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2025-12-31 01:32:32 +0900

    [Gold III] Title: 스크루지 민호 2, Time: 452 ms, Memory: 56004 KB -BaekjoonHub

--- 백준/Gold/12978. 스크루지 민호 2/스크루지 민호 2.java
+++ 백준/Gold/12978. 스크루지 민호 2/스크루지 민호 2.java
@@ -26,7 +26,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
 		bridge = new ArrayList<>();
 		for (int i = 0; i <= n; i++) {
 			bridge.add(new ArrayList<>());
