85b8635fb3f85bf7eb5af359491f10ea2cd805fc
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-01-23 03:03:35 +0900

    [Silver V] Title: 팔찌 만들기, Time: 112 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Silver/25707. 팔찌 만들기/팔찌 만들기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        long[] arr = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        long answer = 2 * (arr[N - 1] - arr[0]);
+        System.out.println(answer);
+    }
+}
