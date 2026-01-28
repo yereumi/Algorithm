195a5441b06d813f9b48f1de3df5f0bf66758370
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-01-29 00:33:14 +0900

    [Gold III] Title: 파티, Time: 444 ms, Memory: 66748 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,63 +1,80 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) >= 48)
-			n = (n << 3) + (n << 1) + (c & 15);
-		return n;
-	}
 	
-	static int n, m;
+	static final long INF = Long.MAX_VALUE;
+	
+	static int N, M, X;
 	static List<List<int[]>> graph;
 	
-	static int dijkstra(int start, int end) {
+	static long[] dijkstra(int start) {
+		long[] dist = new long[N + 1];
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
+		
 		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-		boolean[] visited = new boolean[n + 1];
-		int[] dist = new int[n + 1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
 		pq.offer(new int[] { start, 0 });
 		
 		while (!pq.isEmpty()) {
-			int[] now = pq.poll();
-			if (visited[now[0]]) continue;
-			visited[now[0]] = true;
-			dist[start] = 0;
+			int[] cur = pq.poll();
 			
-			for (int[] next : graph.get(now[0])) {
-				if (!visited[next[0]] && dist[next[0]] > dist[now[0]] + next[1]) {
-					dist[next[0]] = dist[now[0]] + next[1];
-					pq.offer(new int[] { next[0], dist[next[0]] });
+			int curNode = cur[0];
+			int curDist = cur[1];
+			
+			if (dist[curNode] < curDist) continue;
+			
+			for (int[] next : graph.get(curNode)) {
+				int nextNode = next[0];
+				int nextDist = next[1] + curDist;
+				
+				if (dist[nextNode] > nextDist) {
+					dist[nextNode] = nextDist;
+					pq.offer(new int[] { nextNode, nextDist });
 				}
 			}
 		}
-		return dist[end];
+		
+		return dist;
 	}
-
+	
 	public static void main(String[] args) throws Exception {
-		n = read();
-		m = read();
-		int x = read();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		
 		graph = new ArrayList<>();
-		for (int i = 0; i <= n; i++) {
-			graph.add(new ArrayList<>());		
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
 		}
-		for (int i = 0; i < m; i++) {
-			int s = read();
-			int e = read();
-			int w = read();
-			graph.get(s).add(new int[] { e, w });
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int time = Integer.parseInt(st.nextToken());
+			
+			graph.get(start).add(new int[] { end, time });
 		}
-		for (int i = 1; i <= n; i++) {
-			Collections.sort(graph.get(i), (o1, o2) -> o1[1] - o2[1]);
+		
+		long[] depart = new long[N + 1];
+		for (int i = 1; i <= N; i++) {
+			long[] dist = dijkstra(i);
+			depart[i] = dist[X];
 		}
-		int max = 0;
-		for (int i = 1; i <= n; i++) {
-			if (i == x) continue;
-			max = Math.max(max, dijkstra(i, x) + dijkstra(x, i));
+		
+		long[] arrive = dijkstra(X);
+		long[] result = new long[N + 1];
+		
+		long answer = 0;
+		for (int i = 1; i <= N; i++) {
+			result[i] = depart[i] + arrive[i];
+			answer = Math.max(answer, result[i]);
 		}
-		System.out.println(max);
+		
+		System.out.println(answer);
+		br.close();
 	}
 }
