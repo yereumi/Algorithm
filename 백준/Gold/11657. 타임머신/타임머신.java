28433e0a1652818a2fc25e7ef86f12f5b740d32d
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-01-30 00:29:45 +0900

    [Gold IV] Title: 타임머신, Time: 256 ms, Memory: 19556 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -2,62 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int n, m;
-	static int[][] edges;
+
+	static final long INF = Long.MAX_VALUE;
+	static final int START = 1;
+
+	static int N, M;
+	static List<int[]> edges;
 	static long[] dist;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		edges = new int[m][3];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		for (int i = 0; i < m; i++) {
+		dist = new long[N + 1];
+		Arrays.fill(dist, INF);
+		dist[START] = 0;
+		
+		edges = new ArrayList<>();
+		
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			edges[i][0] = Integer.parseInt(st.nextToken());
-			edges[i][1] = Integer.parseInt(st.nextToken());
-			edges[i][2] = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			edges.add(new int[] { a, b, c });
 		}
 		
-		dist = new long[n + 1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[1] = 0;
-		
-		for (int i = 0; i < n - 1; i++) {
-			for (int[] e : edges) {
-				int s = e[0];
-				int d = e[1];
-				int w = e[2];
+		for (int i = 0; i < N - 1; i++) {
+			for (int[] edge : edges) {
+				int from = edge[0];
+				int to = edge[1];
+				int cost = edge[2];
 				
-				if (dist[s] != Integer.MAX_VALUE && dist[s] + w < dist[d]) {
-					dist[d] = dist[s] + w;
+				if (dist[from] == INF) continue;
+				
+				if (dist[to] > dist[from] + cost) {
+					dist[to] = dist[from] + cost;
 				}
 			}
 		}
 		
-		// 음수 간선 사이클 확인
-		for (int[] e : edges) {
-			int s = e[0];
-			int d = e[1];
-			int w = e[2];
+		for (int[] edge : edges) {
+			int from = edge[0];
+			int to = edge[1];
+			int cost = edge[2];
 			
-			if (dist[s] != Integer.MAX_VALUE && dist[s] + w < dist[d]) {
+			if (dist[from] == INF) continue;
+			
+			if (dist[to] > dist[from] + cost) {
 				System.out.println(-1);
 				return;
 			}
 		}
-
-		StringBuilder sb = new StringBuilder();
 		
-		for (int i = 2; i <= n; i++) {
-			if (dist[i] == Integer.MAX_VALUE) {
-				sb.append(-1).append("\n");
-			} else {
-				sb.append(dist[i]).append("\n");
-			}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 2; i <= N; i++) {
+			if (dist[i] == INF) sb.append(-1);
+			else sb.append(dist[i]);
+			sb.append("\n");
 		}
-		System.out.println(sb);
+		
+		System.out.println(sb.toString());
+		br.close();
 	}
 }
