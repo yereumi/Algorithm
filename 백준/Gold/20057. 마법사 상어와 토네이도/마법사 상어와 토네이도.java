40b57381e4a77372d180fe32986d2eef6e8be838
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-02-03 21:14:39 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 452 ms, Memory: 36268 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -2,266 +2,111 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
+
 	static int N, outSand;
 	static int[][] board;
-	
+	static int[] dr = { 0, 1, 0, -1 };
+	static int[] dc = { -1, 0, 1, 0 };
+
+	// {dr, dc, percent}
+	static int[][][] sandPattern = {
+	    { // 왼쪽
+	        {-1,  1, 1}, { 1,  1, 1},
+	        {-2,  0, 2}, { 2,  0, 2},
+	        {-1,  0, 7}, { 1,  0, 7},
+	        {-1, -1,10}, { 1, -1,10},
+	        { 0, -2, 5}
+	    },
+	    { // 아래쪽
+	        {-1, -1, 1}, {-1,  1, 1},
+	        { 0, -2, 2}, { 0,  2, 2},
+	        { 0, -1, 7}, { 0,  1, 7},
+	        { 1, -1,10}, { 1,  1,10},
+	        { 2,  0, 5}
+	    },
+	    { // 오른쪽
+	        {-1, -1, 1}, { 1, -1, 1},
+	        {-2,  0, 2}, { 2,  0, 2},
+	        {-1,  0, 7}, { 1,  0, 7},
+	        {-1,  1,10}, { 1,  1,10},
+	        { 0,  2, 5}
+	    },
+	    { // 위쪽
+	        { 1, -1, 1}, { 1,  1, 1},
+	        { 0, -2, 2}, { 0,  2, 2},
+	        { 0, -1, 7}, { 0,  1, 7},
+	        {-1, -1,10}, {-1,  1,10},
+	        {-2,  0, 5}
+	    }
+	};
+
 	static boolean isValid(int r, int c) {
 		return r >= 0 && r < N && c >= 0 && c < N;
 	}
-	
-	static void moveLeft(int r, int c, int sand) {
-		int sand1 = (int) (sand * 0.01);
-		if (isValid(r - 1, c + 1)) {
-			board[r - 1][c + 1] += sand1;
-		} else outSand += sand1;
-		if (isValid(r + 1, c + 1)) {
-			board[r + 1][c + 1] += sand1;
-		} else outSand += sand1;
-		
-		int sand2 = (int) (sand * 0.02);
-		if (isValid(r - 2, c)) {
-			board[r - 2][c] += sand2;
-		} else outSand += sand2;
-		if (isValid(r + 2, c)) {
-			board[r + 2][c] += sand2;
-		} else outSand += sand2;
-		
-		int sand7 = (int) (sand * 0.07);
-		if (isValid(r - 1, c)) {
-			board[r - 1][c] += sand7;
-		} else outSand += sand7;
-		if (isValid(r + 1, c)) {
-			board[r + 1][c] += sand7;
-		} else outSand += sand7;
-		
-		int sand10 = (int) (sand * 0.1);
-		if (isValid(r - 1, c - 1)) {
-			board[r - 1][c - 1] += sand10;
-		} else outSand += sand10;
-		if (isValid(r + 1, c - 1)) {
-			board[r + 1][c - 1] += sand10;
-		} else outSand += sand10;
-		
-		int sand5 = (int) (sand * 0.05);
-		if (isValid(r, c - 2)) {
-			board[r][c - 2] += sand5;
-		} else outSand += sand5;
-		
-		int total = sand1 * 2 + sand2 * 2 + sand7 * 2 + sand10 * 2 + sand5;
-		board[r][c] = 0;
-		if (isValid(r, c - 1)) {
-			board[r][c - 1] += sand - total;
-		} else outSand += sand - total;
+
+	static void move(int r, int c, int dir) {
+	    int sand = board[r][c];
+	    int total = 0;
+
+	    for (int[] p : sandPattern[dir]) {
+	        int nr = r + p[0];
+	        int nc = c + p[1];
+	        int amount = sand * p[2] / 100;
+	        total += amount;
+
+	        if (isValid(nr, nc)) board[nr][nc] += amount;
+	        else outSand += amount;
+	    }
+
+	    int ar = r + dr[dir];
+	    int ac = c + dc[dir];
+	    int remain = sand - total;
+
+	    if (isValid(ar, ac)) board[ar][ac] += remain;
+	    else outSand += remain;
+
+	    board[r][c] = 0;
 	}
-	
-	static void moveRight(int r, int c, int sand) {
-		int sand1 = (int) (sand * 0.01);
-		if (isValid(r - 1, c - 1)) {
-			board[r - 1][c - 1] += sand1;
-		} else outSand += sand1;
-		if (isValid(r + 1, c - 1)) {
-			board[r + 1][c - 1] += sand1;
-		} else outSand += sand1;
-		
-		int sand2 = (int) (sand * 0.02);
-		if (isValid(r - 2, c)) {
-			board[r - 2][c] += sand2;
-		} else outSand += sand2;
-		if (isValid(r + 2, c)) {
-			board[r + 2][c] += sand2;
-		} else outSand += sand2;
-		
-		int sand7 = (int) (sand * 0.07);
-		if (isValid(r - 1, c)) {
-			board[r - 1][c] += sand7;
-		} else outSand += sand7;
-		if (isValid(r + 1, c)) {
-			board[r + 1][c] += sand7;
-		} else outSand += sand7;
-		
-		int sand10 = (int) (sand * 0.1);
-		if (isValid(r - 1, c + 1)) {
-			board[r - 1][c + 1] += sand10;
-		} else outSand += sand10;
-		if (isValid(r + 1, c + 1)) {
-			board[r + 1][c + 1] += sand10;
-		} else outSand += sand10;
-		
-		int sand5 = (int) (sand * 0.05);
-		if (isValid(r, c + 2)) {
-			board[r][c + 2] += sand5;
-		} else outSand += sand5;
-		
-		int total = sand1 * 2 + sand2 * 2 + sand7 * 2 + sand10 * 2 + sand5;
-		board[r][c] = 0;
-		if (isValid(r, c + 1)) {
-			board[r][c + 1] += sand - total;
-		} else outSand += sand - total;;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int r = N / 2;
+		int c = N / 2;
+		int time = 1;
+		int dir = 0;
+		outSand = 0;
+
+		while (true) {
+		    for (int t = 0; t < 2; t++) {
+		        for (int i = 0; i < time; i++) {
+		            r += dr[dir];
+		            c += dc[dir];
+		            move(r, c, dir);
+		            if (r == 0 && c == 0) {
+		                System.out.println(outSand);
+		                return;
+		            }
+		        }
+		        dir = (dir + 1) % 4;
+		    }
+		    time++;
+		    
+		    if (r == 0 && c == 0) {
+                System.out.println(outSand);
+                break;
+            }
+		}
+
+		br.close();
 	}
-	
-	static void moveDown(int r, int c, int sand) {
-		int sand1 = (int) (sand * 0.01);
-		if (isValid(r - 1, c + 1)) {
-			board[r - 1][c + 1] += sand1;
-		} else outSand += sand1;
-		if (isValid(r - 1, c - 1)) {
-			board[r - 1][c - 1] += sand1;
-		} else outSand += sand1;
-		
-		int sand2 = (int) (sand * 0.02);
-		if (isValid(r, c - 2)) {
-			board[r][c - 2] += sand2;
-		} else outSand += sand2;
-		if (isValid(r, c + 2)) {
-			board[r][c + 2] += sand2;
-		} else outSand += sand2;
-		
-		int sand7 = (int) (sand * 0.07);
-		if (isValid(r, c - 1)) {
-			board[r][c - 1] += sand7;
-		} else outSand += sand7;
-		if (isValid(r, c + 1)) {
-			board[r][c + 1] += sand7;
-		} else outSand += sand7;
-		
-		int sand10 = (int) (sand * 0.1);
-		if (isValid(r + 1, c - 1)) {
-			board[r + 1][c - 1] += sand10;
-		} else outSand += sand10;
-		if (isValid(r + 1, c + 1)) {
-			board[r + 1][c + 1] += sand10;
-		} else outSand += sand10;
-		
-		int sand5 = (int) (sand * 0.05);
-		if (isValid(r + 2, c)) {
-			board[r + 2][c] += sand5;
-		} else outSand += sand5;
-		
-		int total = sand1 * 2 + sand2 * 2 + sand7 * 2 + sand10 * 2 + sand5;
-		board[r][c] = 0;
-		if (isValid(r + 1, c)) {
-			board[r + 1][c] += sand - total;
-		} else outSand += sand - total;
-	}
-	
-	static void moveUp(int r, int c, int sand) {
-		int sand1 = (int) (sand * 0.01);
-		if (isValid(r + 1, c + 1)) {
-			board[r + 1][c + 1] += sand1;
-		} else outSand += sand1;
-		if (isValid(r + 1, c - 1)) {
-			board[r + 1][c - 1] += sand1;
-		} else outSand += sand1;
-		
-		int sand2 = (int) (sand * 0.02);
-		if (isValid(r, c - 2)) {
-			board[r][c - 2] += sand2;
-		} else outSand += sand2;
-		if (isValid(r, c + 2)) {
-			board[r][c + 2] += sand2;
-		} else outSand += sand2;
-		
-		int sand7 = (int) (sand * 0.07);
-		if (isValid(r, c - 1)) {
-			board[r][c - 1] += sand7;
-		} else outSand += sand7;
-		if (isValid(r, c + 1)) {
-			board[r][c + 1] += sand7;
-		} else outSand += sand7;
-		
-		int sand10 = (int) (sand * 0.1);
-		if (isValid(r - 1, c - 1)) {
-			board[r - 1][c - 1] += sand10;
-		} else outSand += sand10;
-		if (isValid(r - 1, c + 1)) {
-			board[r - 1][c + 1] += sand10;
-		} else outSand += sand10;
-		
-		int sand5 = (int) (sand * 0.05);
-		if (isValid(r - 2, c)) {
-			board[r - 2][c] += sand5;
-		} else outSand += sand5;
-		
-		int total = sand1 * 2 + sand2 * 2 + sand7 * 2 + sand10 * 2 + sand5;
-		board[r][c] = 0;
-		if (isValid(r - 1, c)) {
-			board[r - 1][c] += sand - total;
-		} else outSand += sand - total;
-	}
-	
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	N = Integer.parseInt(br.readLine());
-    	board = new int[N][N];
-    	StringTokenizer st;
-        for (int i = 0; i < N; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	for (int j = 0; j < N; j++) {
-        		board[i][j] = Integer.parseInt(st.nextToken());
-        	}
-        }
-        
-        int r = N / 2;
-        int c = N / 2;
-        int time = 1;
-        outSand = 0;
-        
-        while (true) {
-        	// 왼쪽 이동
-        	for (int i = 0; i < time; i++) {
-        		c--;
-        		int sand = board[r][c];
-        		moveLeft(r, c, sand);
-        		if (r == 0 && c == 0) {
-        			System.out.println(outSand);
-        			return;
-        		};
-        	}
-        	
-        	// 아래쪽 이동
-        	for (int i = 0; i < time; i++) {
-        		r++;
-        		int sand = board[r][c];
-        		moveDown(r, c, sand);
-        		if (r == 0 && c == 0) {
-        			System.out.println(outSand);
-        			return;
-        		};
-        	}
-        	
-        	time++;
-        	
-        	// 오른쪽 이동
-        	for (int i = 0; i < time; i++) {
-        		c++;
-        		int sand = board[r][c];
-        		moveRight(r, c, sand);
-        		if (r == 0 && c == 0) {
-        			System.out.println(outSand);
-        			return;
-        		};
-        	}
-        	
-        	// 위쪽 이동
-        	for (int i = 0; i < time; i++) {
-        		r--;
-        		int sand = board[r][c];
-        		moveUp(r, c, sand);
-        		if (r == 0 && c == 0) {
-        			System.out.println(outSand);
-        			return;
-        		};
-        	}
-        	
-        	time++;
-        	
-        	if (r == 0 && c == 0) {
-    			System.out.println(outSand);
-    			break;
-    		};
-        }
-        
-        br.close();
-    }
 }
