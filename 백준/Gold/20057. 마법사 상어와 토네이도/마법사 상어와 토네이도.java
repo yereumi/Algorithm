5484d55f0d9e50c091df4ff6e14eadbe21859e50
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-02-03 21:19:00 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 444 ms, Memory: 36108 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -10,62 +10,61 @@ public class Main {
 
 	// {dr, dc, percent}
 	static int[][][] sandPattern = {
-	    { // 왼쪽
-	        {-1,  1, 1}, { 1,  1, 1},
-	        {-2,  0, 2}, { 2,  0, 2},
-	        {-1,  0, 7}, { 1,  0, 7},
-	        {-1, -1,10}, { 1, -1,10},
-	        { 0, -2, 5}
-	    },
-	    { // 아래쪽
-	        {-1, -1, 1}, {-1,  1, 1},
-	        { 0, -2, 2}, { 0,  2, 2},
-	        { 0, -1, 7}, { 0,  1, 7},
-	        { 1, -1,10}, { 1,  1,10},
-	        { 2,  0, 5}
-	    },
-	    { // 오른쪽
-	        {-1, -1, 1}, { 1, -1, 1},
-	        {-2,  0, 2}, { 2,  0, 2},
-	        {-1,  0, 7}, { 1,  0, 7},
-	        {-1,  1,10}, { 1,  1,10},
-	        { 0,  2, 5}
-	    },
-	    { // 위쪽
-	        { 1, -1, 1}, { 1,  1, 1},
-	        { 0, -2, 2}, { 0,  2, 2},
-	        { 0, -1, 7}, { 0,  1, 7},
-	        {-1, -1,10}, {-1,  1,10},
-	        {-2,  0, 5}
-	    }
-	};
+			{ // 왼쪽
+				{ -1, 1, 1 }, { 1, 1, 1 },
+				{ -2, 0, 2 }, { 2, 0, 2 },
+				{ -1, 0, 7 }, { 1, 0, 7 },
+				{ -1, -1, 10 }, { 1, -1, 10 },
+				{ 0, -2, 5 } },
+			{ // 아래쪽
+				{ -1, -1, 1 }, { -1, 1, 1 },
+				{ 0, -2, 2 }, { 0, 2, 2 },
+				{ 0, -1, 7 }, { 0, 1, 7 },
+				{ 1, -1, 10 }, { 1, 1, 10 },
+				{ 2, 0, 5 } },
+			{ // 오른쪽
+				{ -1, -1, 1 }, { 1, -1, 1 },
+				{ -2, 0, 2 }, { 2, 0, 2 },
+				{ -1, 0, 7 }, { 1, 0, 7 },
+				{ -1, 1, 10 }, { 1, 1, 10 },
+				{ 0, 2, 5 } },
+			{ // 위쪽
+				{ 1, -1, 1 }, { 1, 1, 1 },
+				{ 0, -2, 2 }, { 0, 2, 2 },
+				{ 0, -1, 7 }, { 0, 1, 7 },
+				{ -1, -1, 10 }, { -1, 1, 10 },
+				{ -2, 0, 5 }}};
 
 	static boolean isValid(int r, int c) {
 		return r >= 0 && r < N && c >= 0 && c < N;
 	}
 
 	static void move(int r, int c, int dir) {
-	    int sand = board[r][c];
-	    int total = 0;
+		int sand = board[r][c];
+		int total = 0;
 
-	    for (int[] p : sandPattern[dir]) {
-	        int nr = r + p[0];
-	        int nc = c + p[1];
-	        int amount = sand * p[2] / 100;
-	        total += amount;
+		for (int[] p : sandPattern[dir]) {
+			int nr = r + p[0];
+			int nc = c + p[1];
+			int amount = sand * p[2] / 100;
+			total += amount;
 
-	        if (isValid(nr, nc)) board[nr][nc] += amount;
-	        else outSand += amount;
-	    }
+			if (isValid(nr, nc))
+				board[nr][nc] += amount;
+			else
+				outSand += amount;
+		}
 
-	    int ar = r + dr[dir];
-	    int ac = c + dc[dir];
-	    int remain = sand - total;
+		int ar = r + dr[dir];
+		int ac = c + dc[dir];
+		int remain = sand - total;
 
-	    if (isValid(ar, ac)) board[ar][ac] += remain;
-	    else outSand += remain;
+		if (isValid(ar, ac))
+			board[ar][ac] += remain;
+		else
+			outSand += remain;
 
-	    board[r][c] = 0;
+		board[r][c] = 0;
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -87,24 +86,24 @@ public class Main {
 		outSand = 0;
 
 		while (true) {
-		    for (int t = 0; t < 2; t++) {
-		        for (int i = 0; i < time; i++) {
-		            r += dr[dir];
-		            c += dc[dir];
-		            move(r, c, dir);
-		            if (r == 0 && c == 0) {
-		                System.out.println(outSand);
-		                return;
-		            }
-		        }
-		        dir = (dir + 1) % 4;
-		    }
-		    time++;
-		    
-		    if (r == 0 && c == 0) {
-                System.out.println(outSand);
-                break;
-            }
+			for (int t = 0; t < 2; t++) {
+				for (int i = 0; i < time; i++) {
+					r += dr[dir];
+					c += dc[dir];
+					move(r, c, dir);
+					if (r == 0 && c == 0) {
+						System.out.println(outSand);
+						return;
+					}
+				}
+				dir = (dir + 1) % 4;
+			}
+			time++;
+
+			if (r == 0 && c == 0) {
+				System.out.println(outSand);
+				break;
+			}
 		}
 
 		br.close();
