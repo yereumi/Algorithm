ee4eae7578f4ccfcf86bff25374b053ed5d67577
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-02-06 20:28:59 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 568 ms, Memory: 49900 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,47 +1,65 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	
+	static int V, E;
+	static int[] parent;
+	static List<int[]> graph;
 
+	static int find(int x) {
+		if (parent[x] != x) {
+			parent[x] = find(parent[x]); // 경로 압축
+		}
+		return parent[x];
+	}
+
+	static void union(int a, int b) {
+		int rootA = find(a);
+		int rootB = find(b);
+
+		if (rootA == rootB) return;
+
+		parent[rootB] = rootA;
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int v = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		List<List<int[]>> graph = new ArrayList<>();
-		for (int i = 0; i <= v; i++) {
-			graph.add(new ArrayList<>());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		parent = new int[V + 1];
+		for (int i = 0; i <= V; i++) {
+			parent[i] = i;
 		}
-		for (int i = 1; i <= e; i++) {
+		
+		graph = new ArrayList<>();
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			graph.get(start).add(new int[] { start, end, weight });
-			graph.get(end).add(new int[] { end, start, weight });
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			graph.add(new int[] { a, b, c });
 		}
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-		pq.add(new int[] { 1, 0 });
-		boolean[] visited = new boolean[v + 1];
-		int weightSum = 0;
-		while (!pq.isEmpty()) {
-			int[] now = pq.poll();
-			int u = now[0];
-			int w = now[1];
-
-			if (visited[u]) continue;
-			weightSum += w;
-			visited[u] = true;
-			for (int[] next : graph.get(u)) {
-				int nu = next[1];
-				int nw = next[2];
-				if (!visited[nu]) pq.add(new int[] { nu, nw });
+		
+		Collections.sort(graph, (o1, o2) -> o1[2] - o2[2]);
+		int cost = 0;
+		int cnt = 0;
+		for (int i = 0; i < E; i++) {			
+			int[] cur = graph.get(i);
+			
+			if (find(cur[0]) != find(cur[1])) {
+				union(cur[0], cur[1]);
+				cost += cur[2];
+				cnt++;
+				
+				if (cnt == V - 1) break;
 			}
 		}
-		bw.write(String.valueOf(weightSum));
-		bw.flush();
+		
+		System.out.println(cost);
+		
 		br.close();
-		bw.close();
 	}
 }
