88255631ed92bee87313fc43bf02e361f431f054
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-02-07 21:01:23 +0900

    [Gold V] Title: 회의실 배정, Time: 384 ms, Memory: 43292 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -2,25 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[][] room = new int[n][2];
-		for (int i = 0; i < n; i++) {
+		int N = Integer.parseInt(br.readLine());
+		int[][] meetings = new int[N][2];
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			room[i][0] = Integer.parseInt(st.nextToken());
-			room[i][1] = Integer.parseInt(st.nextToken());
+			meetings[i][0] = Integer.parseInt(st.nextToken());
+			meetings[i][1] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(room, (o1, o2) -> o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0]);
-		int i = 0, cnt = 0, time = 0;
-		while (i < n) {
-			if (time <= room[i][0]) {
-				time = room[i][1];
+		
+		Arrays.sort(meetings, (o1, o2) -> {
+			if (o1[1] != o2[1]) return o1[1] - o2[1];
+			return o1[0] - o2[0];
+		});
+		
+		int lastEndTime = 0;
+		int cnt = 0;
+		
+		for (int[] m : meetings) {
+			int s = m[0];
+			int e = m[1];
+			
+			if (lastEndTime <= s) {
 				cnt++;
+				lastEndTime = e;
 			}
-			i++;
 		}
+		
 		System.out.println(cnt);
+		
+		br.close();
 	}
 }
