763e37ee6f0a0aa149bb303d3b439a2d1bda1ff4
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-02-15 00:08:15 +0900

    [Gold III] Title: 줄 세우기, Time: 364 ms, Memory: 48208 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,43 +1,55 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    
-    private static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) >= 48)
-			n = (n << 3) + (n << 1) + (c & 15);
-		return n;
-	}
-    
+	
+	static int N, M;
+	static List<List<Integer>> graph;
+	static int[] indegree;
+	
 	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		int n = read();
-		int m = read();
-		List<List<Integer>> graph = new ArrayList<>();
-		for (int i = 0; i <= n; i++) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		graph = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<>());
 		}
-		int[] edgeCount = new int[n + 1];
-		for (int i = 0; i < m; i++) {
-			int a = read();
-			int b = read();
+		indegree = new int[N + 1];
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
 			graph.get(a).add(b);
-			edgeCount[b]++;
+			indegree[b]++;
 		}
 		
 		Deque<Integer> dq = new ArrayDeque<>();
-		for (int i = 1; i <= n; i++) {
-			if (edgeCount[i] == 0) dq.offer(i);
+		for (int i = 1; i <= N; i++) {
+			if (indegree[i] == 0) dq.offer(i);
 		}
+		
+		List<Integer> result = new ArrayList<>();
 		while (!dq.isEmpty()) {
-			int v = dq.poll();
-			sb.append(v).append(" ");
-			for (int u : graph.get(v)) {
-				edgeCount[u]--;
-				if (edgeCount[u] == 0) dq.offer(u);
+			int cur = dq.poll();
+			result.add(cur);
+			
+			for (int next : graph.get(cur)) {
+				indegree[next]--;
+				if (indegree[next] == 0) dq.offer(next);
 			}
 		}
-		System.out.println(sb);
+		
+		for (int r : result) {
+			sb.append(r).append(" ");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
 	}
 }
