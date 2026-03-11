9f88daa4de18d44cae4b5837bbbacc1ad0deeb03
Yeji Shin <91780220+yereumi@users.noreply.github.com>
2026-03-11 23:13:48 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static long pow(long a, long b, long c) {
+        if (b == 1) return a % c;
+
+        long half = pow(a, b / 2, c);
+        long result = (half * half) % c;
+
+        if (b % 2 == 1) {
+            result = (result * a) % c;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(pow(A, B, C));
+    }
+}
